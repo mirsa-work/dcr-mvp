c19e550710a506903d26d918c58c9bc411e26e62
Mirsa Mohammed <mirsa.work@gmail.com>
2025-05-06 14:01:14 +0000

    feat: Day 3 – bug fixes

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const role = require('./middleware/roleGuard');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /* public routes */
 app.use('/api', authRoute);
