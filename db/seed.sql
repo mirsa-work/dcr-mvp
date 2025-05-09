b4a7bd0b1153182bd153ce2196b400236cea2810
Mirsa Mohammed <mirsa.work@gmail.com>
2025-05-09 08:30:51 +0000

    Day 5

--- db/seed.sql
+++ db/seed.sql
@@ -7,5 +7,7 @@ INSERT INTO branches (code, name) VALUES
 /* users (password = "pass" hashed with bcrypt) */
 INSERT INTO users (username, password, role, branch_id) VALUES
   ('rak_user',  '$2b$10$amhzyKTicDn9U.lPepyn3.b0nLBTayHY4l2KYGW9CRNo1MkcXlqeC', 'BRANCH', 1),
+  ('aln_user',  '$2b$10$amhzyKTicDn9U.lPepyn3.b0nLBTayHY4l2KYGW9CRNo1MkcXlqeC', 'BRANCH', 2),
+  ('afw_user',  '$2b$10$amhzyKTicDn9U.lPepyn3.b0nLBTayHY4l2KYGW9CRNo1MkcXlqeC', 'BRANCH', 3),
   ('viewer',    '$2b$10$amhzyKTicDn9U.lPepyn3.b0nLBTayHY4l2KYGW9CRNo1MkcXlqeC', 'VIEWER', NULL),
   ('admin',     '$2b$10$amhzyKTicDn9U.lPepyn3.b0nLBTayHY4l2KYGW9CRNo1MkcXlqeC', 'ADMIN',  NULL);
